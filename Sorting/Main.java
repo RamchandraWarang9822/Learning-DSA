37381ab2838e77ad27c062f41c588202fa102f3c
Ramchandra Warang <92023869+RamchandraWarang9822@users.noreply.github.com>
2022-09-08 17:42:54 +0530

    Update Main.java

--- Sorting/Main.java
+++ Sorting/Main.java
@@ -1,38 +1,64 @@
 import java.util.Scanner;
 
 public class Main {
+    
+    static String sorted , unsorted;
+    static Scanner sc = new Scanner(System.in);
+    static int n;
 
-	public void print(int[] arr) {
+    //Printing array
+    static void printArray(int[] arr,String sorted){
 
-		System.out.println("Sorted Array: ");
-		for (int i = 0; i < arr.length; i++) {
+            System.out.print("\n"+sorted);
+            for (int i = 0; i<arr.length;i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println("\n");
+    }
+    
+    
 
-			System.out.print(arr[i] + " ");
+    public static void main(String[] args) {
 
-		}
+        
+        //Takes the size of the array 
+        System.out.print("Enter the size of the array : ");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        
+        //Taking values in array
+        for (int i = 0; i<size;i++) {
+            System.out.print("Enter value for " + i + ":");
+            arr[i] = sc.nextInt();
+        }
+        
+        //printing the unsorted array
+        printArray(arr,sorted = "Array : ");
+        
+        //Objects
+        bubbleSort bubbleSort = new bubbleSort();
+        selectionSort selectionSort = new selectionSort();
+        
 
-	}
-
-	public static void main(String[] args) {
-
-		int[] arr;
-
-		Scanner sc = new Scanner(System.in);
-
-		System.out.print("Enter no. of elements: ");
-		int size = sc.nextInt();
-
-		arr = new int[size];
-
-		System.out.print("Enter elements with spaces in between: ");
-		for (int i = 0; i < size; i++) {
-
-			arr[i] = sc.nextInt();
-
-		}
-
-		bubbleSort b = new bubbleSort(arr);
-		sc.close();
-
-	}
+        //Selecting type of Sorting algorithm
+        System.out.println("Enter number to select Sorting algorithm\n");
+        System.out.println("1 : Bubble Sort");
+        System.out.println("2 : Selection Sort");
+        System.out.println("3 : ");
+        System.out.println("4 : ");
+        System.out.println("5 : ");
+        System.out.println("6 : ");
+        System.out.print("\n=> ");
+        n = sc.nextInt();
+        
+        
+        //running algorithm according the selection
+        switch (n) {
+            case 1: bubbleSort.bubbleSort(arr, sorted);
+            break;
+            case 2: selectionSort.selectionSort(arr, sorted);
+            break;
+        }
+    
+    }
 }
