db921a093621c43f60d63795879ca5e6773f3564
AdityaBabar08 <92012910+AdityaBabar08@users.noreply.github.com>
2022-09-08 10:10:23 +0530

    Update bubbleSort.java

--- Sorting/bubbleSort.java
+++ Sorting/bubbleSort.java
@@ -1,45 +1,42 @@
-public class bubbleSort extends array{
+public class bubbleSort extends array {
 
-    array array = new array();
+	bubbleSort(int[] arr) {
 
-    public static void bubbleSort(arr[]){
+//		int[] arr;
+//
+//		array a = new array();
+//		arr = a.getArray();
 
-            //Taking values in array 
-            array.takeArray();
-        
-            
-            //Bubble Sort (Sinking Sort): 
-            // In this sorting algorithm we push the heavy value element to the end of the array.
-            // by swapping two elements with each other until the array is sorted 
-            
-            //Time complexity : O(n^2)
-            
-            
-                //Outer loop
-                for(int i = 0;i < arr.length-1; i++){
-                
-                    //Inner loop
-                    for(int j=0;j<arr.length-i-1;j++){
-                
-                        if(arr[j] > arr[j+1]){
-                
-                            //Swapping
-                            int temp = arr[j];
-                            arr[j] = arr[j+1];
-                            arr[j+1] = temp;
-                
-                        }
-                    }
-                }
-                
-                System.out.print("Sorted : ");
-                
-                for(int a=0;a<arr.length;a++){
-                
-                    System.out.print(arr[a] + " ");
-                
-                }
-                
-                System.out.println("\n");
-            }
+		// Bubble Sort (Sinking Sort):
+		// In this sorting algorithm we push the heavy value element to the end of the
+		// array.
+		// by swapping two elements with each other until the array is sorted
+
+		// Time complexity : O(n^2)
+
+		// Outer loop
+		for (int i = 0; i < arr.length; i++) {
+
+			// Inner loop
+			for (int j = 0; j < arr.length - i - 1; j++) {
+
+				if (arr[j] > arr[j + 1]) {
+
+					// Swapping
+					int temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+
+				}
+			}
+		}
+
+		// Printing Array
+
+		Main m = new Main();
+
+		m.print(arr);
+
+	}
+
 }
