aecfbd65cec3ce78b8cb261fd10478310d507317
Ramchandra Warang <92023869+RamchandraWarang9822@users.noreply.github.com>
2022-09-13 00:08:50 +0530

    Update selectionSort.java

--- Sorting/selectionSort.java
+++ Sorting/selectionSort.java
@@ -3,14 +3,28 @@ public class selectionSort {
     void selectionSort(int[] arr, String sorted){
 
         //Selection Sort :
-        //
+        //In this sorting algorithm we select and set a value as temp maximum
+        //compare it with each value in array to find the actual true maximum value 
+        //shift the maximum to the end
         
-        //Time complexity : 
+        //Time complexity : O(n^2)
 
-        //
-        
-        //Printing the Sorted array
-        Main.printArray(arr,sorted = "Sorted Array : ");
+        //Outer loop : no. of iterations
+        for(int i = 0; i<arr.length; i++){
+            int smallest = i;
+
+            //Inner loop : no. of comparision and update in smallest 
+            for( int j = i+1; j<arr.length; j++){
+                //Updating the smallest value before swapping
+                if( arr[smallest] > arr[j]){
+                    smallest = j; 
+                }
+            }
+            //Swapping
+            int temp = arr[smallest];
+            arr[smallest] = arr[i];
+            arr[i] = temp;
+        }
+        Main.printArray(arr, sorted = "Selection Sorted Array : ");
     }
-
 }
